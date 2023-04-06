b048494ff317eda319d1b4defdce2b34771f9b90
Marco Senna Fontanari <marcosennafontanari@gmail.com>
2023-04-05 21:30:39 -0300

    Create newtest.sh

---
+++ newtest.sh
@@ -0,0 +1,6 @@
+!#/bin/sh
+mkdir pastaGit
+wget https://img.olhardigital.com.br/wp-content/uploads/2023/04/shutterstock_1196626786-1-1536x682.jpg 
+mv shutterstock_1196626786-1-1536x682.jpg /pastaGit
+ls -la
+pwd
